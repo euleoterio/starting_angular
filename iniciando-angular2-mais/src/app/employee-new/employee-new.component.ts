e81ec0bd89b1f0e5425cdcdaadc6b758a485ceb8
Euleoterio <vinicius.euleoterio@hotmail.com>
2021-09-13 20:26:25 -0300

    Curso Angular 7

---
+++ iniciando-angular2-mais/src/app/employee-new/employee-new.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import employees from '../employees';
+import { Employee, EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'employee-new',
+  templateUrl: './employee-new.component.html',
+  styleUrls: ['./employee-new.component.css']
+})
+export class EmployeeNewComponent implements OnInit {
+
+  employee: Employee = {
+    name: '',
+    salary: 0,
+    bonus: 0
+  };
+  // employees = employees;
+
+  constructor(private employeeService: EmployeeService) {
+    // setTimeout(() => {
+    //   this.name = 'School of Net';
+    // }, 3000);
+  }
+
+  ngOnInit() {
+  }
+
+  addEmployee(event) {
+    const copy = Object.assign({}, this.employee);
+    this.employeeService.addEmployee(this.employee);
+  }
+
+}
---
+++ iniciando-angular2-mais/src/app/employees.ts
@@ -0,0 +1,9 @@
+interface Employee {
+  name: String;
+  salary: number;
+  bonus: number;
+}
+
+const employees: Employee[] = [];
+
+export default employees;
